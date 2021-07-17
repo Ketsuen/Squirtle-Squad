022007705f7f90e474784abc256092a1a8d5d858
Ketsuen <kevin.git@outlook.fr>
2021-07-17 15:11:37 +0200

    maj

--- app.js
+++ app.js
@@ -28,53 +28,57 @@ let stableListe = {
 let numSite = 0;
 client.on("message", (message) => {
   setInterval(async () => {
-    let website = numSite % Object.values(stableListe).length;
-    if (website == 0)
-      stableListe.keytwo = await Keytwo(client, message, stableListe.keytwo);
-    else if (website == 1)
-      stableListe.maitrerenard = await MaitreRenard(
-        client,
-        message,
-        stableListe.maitrerenard
-      );
-    else if (website == 2)
-      stableListe.pikestorep1preco = await Pikastorep1preco(
-        client,
-        message,
-        stableListe.pikestorep1preco
-      );
-    else if (website == 3)
-      stableListe.pikestorep2preco = await Pikastorep2preco(
-        client,
-        message,
-        stableListe.pikestorep2preco
-      );
-    else if (website == 4)
-      stableListe.pikastorep1disponible = await Pikastorep1dispo(
-        client,
-        message,
-        stableListe.pikastorep1disponible
-      );
-    else if (website == 5)
-      stableListe.pokemart = await Pokemart(
-        client,
-        message,
-        stableListe.pokemart
-      );
-    else if (website == 6)
-      stableListe.jelowstore = await JelowStore(
-        client,
-        message,
-        stableListe.jelowstore
-      );
-    else if (website == 7)
-      stableListe.maxitoys = await Maxitoys(
-        client,
-        message,
-        stableListe.maxitoys
-      );
+    try {
+      let website = numSite % Object.values(stableListe).length;
+      if (website == 0)
+        stableListe.keytwo = await Keytwo(client, message, stableListe.keytwo);
+      else if (website == 1)
+        stableListe.maitrerenard = await MaitreRenard(
+          client,
+          message,
+          stableListe.maitrerenard
+        );
+      else if (website == 2)
+        stableListe.pikestorep1preco = await Pikastorep1preco(
+          client,
+          message,
+          stableListe.pikestorep1preco
+        );
+      else if (website == 3)
+        stableListe.pikestorep2preco = await Pikastorep2preco(
+          client,
+          message,
+          stableListe.pikestorep2preco
+        );
+      else if (website == 4)
+        stableListe.pikastorep1disponible = await Pikastorep1dispo(
+          client,
+          message,
+          stableListe.pikastorep1disponible
+        );
+      else if (website == 5)
+        stableListe.pokemart = await Pokemart(
+          client,
+          message,
+          stableListe.pokemart
+        );
+      else if (website == 6)
+        stableListe.jelowstore = await JelowStore(
+          client,
+          message,
+          stableListe.jelowstore
+        );
+      else if (website == 7)
+        stableListe.maxitoys = await Maxitoys(
+          client,
+          message,
+          stableListe.maxitoys
+        );
+    } catch (error) {
+      console.log(error);
+    }
     numSite++;
-  }, 7000);
+  }, 10000);
 });
 // client.channels.cache.get("862724611246522388D").send("hello world");
 client.login(process.env.TOKEN);
