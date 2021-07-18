c81aee338157d57b7245529be7b807ee4a09622e
Ketsuen <kevin.git@outlook.fr>
2021-07-18 22:17:21 +0200

    maj

--- app.js
+++ app.js
@@ -58,112 +58,112 @@ client.on("message", (message) => {
         if (website == 0) {
           if (stableListe.keytwo.actif) {
             stableListe.keytwo.actif = false;
-            let tempArray = [];
-            let tempArray = await Keytwo(
+            let tempArray1 = [];
+            let tempArray1 = await Keytwo(
               client,
               message,
               stableListe.keytwo.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.keytwo.tableau = tempArray;
+            if (tempArray1.length > 0) {
+              stableListe.keytwo.tableau = tempArray1;
               stableListe.keytwo.actif = true;
             }
           }
         } else if (website == 1) {
           if (stableListe.maitrerenard.actif) {
             stableListe.maitrerenard.actif = false;
-            let tempArray = [];
-            tempArray = await MaitreRenard(
+            let tempArray2 = [];
+            tempArray2 = await MaitreRenard(
               client,
               message,
               stableListe.maitrerenard.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.maitrerenard.tableau = tempArray;
+            if (tempArray2.length > 0) {
+              stableListe.maitrerenard.tableau = tempArray2;
               stableListe.maitrerenard.actif = true;
             }
           }
         } else if (website == 2) {
           if (stableListe.pikestorep1preco.actif) {
             stableListe.pikestorep1preco.actif = false;
-            let tempArray = [];
-            tempArray = await Pikastorep1preco(
+            let tempArray3 = [];
+            tempArray3 = await Pikastorep1preco(
               client,
               message,
               stableListe.pikestorep1preco.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.pikestorep1preco.tableau = tempArray;
+            if (tempArray3.length > 0) {
+              stableListe.pikestorep1preco.tableau = tempArray3;
               stableListe.pikestorep1preco.actif = true;
             }
           }
         } else if (website == 3) {
           if (stableListe.pikestorep2preco.actif) {
             stableListe.pikestorep2preco.actif = false;
-            let tempArray = [];
-            tempArray = await Pikastorep2preco(
+            let tempArray4 = [];
+            tempArray4 = await Pikastorep2preco(
               client,
               message,
               stableListe.pikestorep2preco.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.pikestorep2preco.tableau = tempArray;
+            if (tempArray4.length > 0) {
+              stableListe.pikestorep2preco.tableau = tempArray4;
               stableListe.pikestorep2preco.actif = true;
             }
           }
         } else if (website == 4) {
           if (stableListe.pikastorep1disponible.actif) {
             stableListe.pikastorep1disponible.actif = false;
-            let tempArray = [];
-            tempArray = await Pikastorep1dispo(
+            let tempArray5 = [];
+            tempArray5 = await Pikastorep1dispo(
               client,
               message,
               stableListe.pikastorep1disponible.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.pikastorep1disponible.tableau = tempArray;
+            if (tempArray5.length > 0) {
+              stableListe.pikastorep1disponible.tableau = tempArray5;
               stableListe.pikastorep1disponible.actif = true;
             }
           }
         } else if (website == 5) {
           if (stableListe.pokemart.actif) {
             stableListe.pokemart.actif = false;
-            let tempArray = [];
-            tempArray = await Pokemart(
+            let tempArray6 = [];
+            tempArray6 = await Pokemart(
               client,
               message,
               stableListe.pokemart.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.pokemart.tableau = tempArray;
+            if (tempArray6.length > 0) {
+              stableListe.pokemart.tableau = tempArray6;
               stableListe.pokemart.actif = true;
             }
           }
         } else if (website == 6) {
           if (stableListe.jelowstore.actif) {
             stableListe.jelowstore.actif = false;
-            let tempArray = [];
-            tempArray = await JelowStore(
+            let tempArray7 = [];
+            tempArray7 = await JelowStore(
               client,
               message,
               stableListe.jelowstore.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.jelowstore.tableau = tempArray;
+            if (tempArray7.length > 0) {
+              stableListe.jelowstore.tableau = tempArray7;
               stableListe.jelowstore.actif = true;
             }
           }
         } else if (website == 7) {
           if (stableListe.maxitoys.actif) {
             stableListe.maxitoys.actif = false;
-            let tempArray = [];
-            tempArray = await Maxitoys(
+            let tempArray8 = [];
+            tempArray8 = await Maxitoys(
               client,
               message,
               stableListe.maxitoys.tableau
             );
-            if (tempArray.length > 0) {
-              stableListe.maxitoys.tableau = tempArray;
+            if (tempArray8.length > 0) {
+              stableListe.maxitoys.tableau = tempArray8;
               stableListe.maxitoys.actif = true;
             }
           }
