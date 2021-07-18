82b8da96cb2b231850974a1fbeb75cf8f3b3d5eb
Ketsuen <kevin.git@outlook.fr>
2021-07-19 00:36:57 +0200

    maj

--- maitrerenard.js
+++ maitrerenard.js
@@ -13,45 +13,49 @@ async function MaitreRenard(client, message, stableListe) {
     "https://maitrerenard.shop/univers/cartes-a-collectionner/?min_price=14&max_price=100&count=50",
     { waitUntil: "load", timeout: 0 }
   );
+  try {
+    const listeArticles = await page.evaluate(() => {
+      let listeArticles = [];
+      let elements = document.querySelectorAll("ul li.product");
+      for (element of elements) {
+        listeArticles.push({
+          name: element.querySelector("div.product-content h3").textContent,
+          dispo: !element.querySelector("a").querySelector("div.out-of-stock")
+            ? "dispo"
+            : "pas dispo",
+          url: element.querySelector("div.product-image a").href,
+        });
+      }
 
-  const listeArticles = await page.evaluate(() => {
-    let listeArticles = [];
-    let elements = document.querySelectorAll("ul li.product");
-    for (element of elements) {
-      listeArticles.push({
-        name: element.querySelector("div.product-content h3").textContent,
-        dispo: !element.querySelector("a").querySelector("div.out-of-stock")
-          ? "dispo"
-          : "pas dispo",
-        url: element.querySelector("div.product-image a").href,
-      });
+      return listeArticles;
+    });
+    var newArticles = await listeArticles.filter(
+      await comparer(stableListe.tableau)
+    );
+    for (let index = 0; index < newArticles.length; index++) {
+      if (
+        newArticles[index].dispo === "dispo" &&
+        stableListe.tableau.length > 0
+      ) {
+        message.channel.send(
+          "MAITRE RENARD\n" +
+            newArticles[index].name +
+            "\n" +
+            newArticles[index].url
+        );
+      }
     }
+
+    stableListe.actif = true;
+    await page.close();
+    await browser.close();
+    console.log("maitrerenard" + listeArticles.length);
 
     return listeArticles;
-  });
-  var newArticles = await listeArticles.filter(
-    await comparer(stableListe.tableau)
-  );
-  for (let index = 0; index < newArticles.length; index++) {
-    if (
-      newArticles[index].dispo === "dispo" &&
-      stableListe.tableau.length > 0
-    ) {
-      message.channel.send(
-        "MAITRE RENARD\n" +
-          newArticles[index].name +
-          "\n" +
-          newArticles[index].url
-      );
-    }
+  } catch (error) {
+    stableListe.actif = true;
+    console.log(error);
   }
-
-  stableListe.actif = true;
-  await page.close();
-  await browser.close();
-  console.log("maitrerenard" + listeArticles.length);
-
-  return listeArticles;
 }
 
 module.exports = MaitreRenard;
